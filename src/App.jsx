ee41150113f6b7600dd43e32ed8a4b6b4a434cc5
Afraz Mishak <afrazmishak@gmail.com>
2025-11-13 12:57:09 +0400

    Temporary Update for Lunch Break

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import { useState } from 'react';
 import './App.css'
+
+function WelcomeBanner () {
+  return (
+    <div className="WelcomeBannerContainer">
+      <input type="image" src="./src/assets/Assalamu Alaikum Banner.png" alt="" />
+    </div>
+  )
+}
+
+function MenuBar () {
+  return (
+    <>
+      <ul className="HomeMenuBar">
+        <li className="HomeMenuBarListItems">Home</li>
+        <li className="HomeMenuBarListItems">Home</li>
+        <li className="HomeMenuBarListItems">Home</li>
+        <li className="HomeMenuBarListItems"><a href="#" className="HomeMenuBarListItemsARef">Home</a></li>
+      </ul>
+    </>
+  )
+}
 
 function Astagfirullah() {
     return (
-    <button className="button-counter" onClick={ZikrButton}>
+    <button className="button-counter">
         <p className="line-one-arabic">أستغفر الله</p>
         <span className="line-one-english">"I ask God for forgiveness"</span>
     </button>
@@ -19,8 +39,8 @@ function Count() {
 function App() {
   return (
     <>
-      <Astagfirullah />
-      <Count />
+      <WelcomeBanner />
+      <MenuBar />
     </>
   )
 }
